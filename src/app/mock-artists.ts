f204bf42aa1d2e7d5e1b0560336d309b548d0157
Grinil Khanna and Shruti Priya <grinil+shruti@>
2017-04-04 10:56:12 -0700

    Add database ghardcoded.

---
+++ src/app/mock-artists.ts
@@ -0,0 +1,15 @@
+import { Artist } from './artist.model';
+
+export let ARTISTS: Artist[] = [
+
+    new Artist('Cameo'),
+    new Artist('Ohio Players'),
+    new Artist('Lakeside'),
+    new Artist('Myles Davis'),
+    new Artist('John Coltraine'),
+    new Artist('Louis Armstrong'),
+    new Artist('Michael Jackson'),
+    new Artist('Lady Gaga'),
+    new Artist('Pharrel')
+
+];
---
+++ src/app/mock-genres.ts
@@ -0,0 +1,8 @@
+import { Genre } from './genre.model';
+
+export let GENRES: Genre[] = [
+
+    new Genre('Funk');
+    new Genre('Pop');
+    new Genre('Jazz'); 
+];
